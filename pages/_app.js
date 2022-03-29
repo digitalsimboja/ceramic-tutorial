65c80d02260e19763d06e47f6ee9df3d29d7e0ff
Sunday Mgbogu <digitalsimboja@gmail.com>
2022-03-29 05:11:59 +0100

    Initial deploy Ceramic demo

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import { Provider } from '@self.id/react';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider client={{ ceramic: "testnet-clay" }}>
+      <Component {...pageProps} />;
+    </Provider>
+  );
 }
+
 
 export default MyApp
